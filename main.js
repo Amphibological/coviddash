cbc582335e00b8fc322d92c5e8143c635859f81b
Amphibological <james.chiella@gmail.com>
2020-05-13 15:17:17 -0400

    more functionality, now has all data

--- main.js
+++ main.js
@@ -1,57 +1,76 @@
-var results;
-
-function renameObjectKey(oldObj, oldName, newName) {
-    const newObj = {};
-
-    Object.keys(oldObj).forEach(key => {
-        const value = oldObj[key];
-
-        if (key === oldName) {
-            newObj[newName] = value;
-        } else {
-            newObj[key] = value;
-        }
-    });
-
-    return newObj;
-}
+const fields = [
+"Reported Date",
+"Confirmed Negative",
+"Presumptive Negative",
+"Presumptive Positive",
+"Confirmed Positive",
+"Resolved",
+"Deaths",
+"Total Cases",
+"Total patients approved for testing as of Reporting Date",
+"Total tests completed in the last day",
+"Under Investigation",
+"Number of patients hospitalized with COVID-19",
+"Number of patients in ICU with COVID-19",
+"Number of patients in ICU on a ventilator with COVID-19",
+];
 
 $(document).ready(() => {
-
-    var data = {
-        resource_id: 'ed270bb8-340b-41f9-a7c6-e8ef587e6d11', // the resource id
-        fields: "Reported Date,Deaths",
-    };
-    $.ajax({
-        url: 'https://data.ontario.ca/api/3/action/datastore_search',
-        data: data,
-        dataType: 'jsonp',
-        cache: 'true',
-        success: function(data) {
-            results = data.result.records;
-            displayResults();
-        },
-    });
+    fetchData(displayResults);
 });
 
-function displayResults() {
+function fetchData(successHandler) {
+    $.ajax({
+        url: 'https://data.ontario.ca/api/3/action/datastore_search',
+        data: {
+            resource_id: 'ed270bb8-340b-41f9-a7c6-e8ef587e6d11', // the resource id
+        },
+        dataType: 'jsonp',
+        cache: 'true', // do not send timestamp parameter
+        success: successHandler,
+    });
+}
 
-    //console.log(datax)
+function createDataset(info, field, colour) {
+    return {
+        label: field,
+        backgroundColor: colour,
+        data: info.map((item) => {
+            return {x: item["Reported Date"], y: item[field]};
+        }),
+    };
+}
 
-    var ctx = $("#display")[0].getContext('2d');
-    var dispChart = new Chart(ctx, {
+function displayResults(results) {
+    console.log(results);
+
+    results = results.result.records; //get to the actual data
+
+    var context = $("#display")[0].getContext('2d');
+    
+    var dispChart = new Chart(context, {
         type: 'bar',
         data: {
-            //labels: [1, 2, 3, 4, 5, 6, 7, 8],
-            datasets: [{
-                label: "Deaths",
-                backgroundColor: "0x0000ff",
-                data: results.map((item) => {
-                    return {x: item["Reported Date"], y: item["Deaths"]};
-                }),
-            }]
+            datasets: [
+                // {
+                //     label: "Deaths",
+                //     backgroundColor: "0x0000ff",
+                //     data: results.map((item) => {
+                //         return {x: item["Reported Date"], y: item["Deaths"]};
+                //     }),
+                // }
+                //createDataset(results, "Deaths", "#ff0000"),
+            ]
         },
         options: {
+            legend: {
+                labels: {
+                    filter: function(legendItem, chart) {
+                        // Logic to remove a particular legend item goes here
+                        return !legendItem.hidden;
+                    }
+                }
+            },
             scales: {
                 xAxes: [{
                     type: 'time',
@@ -67,4 +86,13 @@ function displayResults() {
             }
         }
     });
+
+    for (fieldNum = 0; fieldNum < fields.length; fieldNum++) {
+        dispChart.data.datasets.push(createDataset(results, fields[fieldNum], "#" + ((Math.trunc(0xffffff/fields.length) * fieldNum)).toString(16)));
+        dispChart.getDatasetMeta(fieldNum).hidden = true;
+    }
+    //dispChart.data.datasets.push(createDataset(results, "Deaths", "#ff0000"));
+    //dispChart.data.datasets.push(createDataset(results, "Total tests completed in the last day", "#00ff00"));
+    //dispChart.getDatasetMeta(0).hidden = true;
+    dispChart.update();
 }
